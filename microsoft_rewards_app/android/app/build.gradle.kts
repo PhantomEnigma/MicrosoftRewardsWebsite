051101523701cd80952ad65abfdb73b0543008d9
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2025-07-25 14:06:58 +0530

    Update Package name

--- microsoft_rewards_app/android/app/build.gradle.kts
+++ microsoft_rewards_app/android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.spin311.microsoft_automatic_rewards"
+        applicationId = "com.spin311.microsoft_automatic_rewards_experimental"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
