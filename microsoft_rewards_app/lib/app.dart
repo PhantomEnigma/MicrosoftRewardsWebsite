4983a348c6f5b3742bc5f6c0a91cc9653ec55ef4
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2025-07-25 11:46:57 +0530

    Dark Theme Support

--- microsoft_rewards_app/lib/app.dart
+++ microsoft_rewards_app/lib/app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
 import 'package:microsoft_automatic_rewards/features/search/presentation/pages/startup_screen.dart';
 import 'core/constants/app_constants.dart';
 import 'core/constants/strings.dart';
 import 'features/search/presentation/bloc/search_bloc.dart';
 import 'core/di/injection_container.dart';
+import 'core/theme/theme_provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context); // Read current theme
+
     return MaterialApp(
       title: Strings.appTitle,
+      themeMode: themeProvider.themeMode, // Enable dynamic switching
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: AppConstants.primary,
@@ -50,6 +56,7 @@ class MyApp extends StatelessWidget {
           linearTrackColor: AppConstants.progressBackground,
         ),
       ),
+      darkTheme: ThemeData.dark(),
       home: BlocProvider(
         create: (context) => sl<SearchBloc>(),
         child: const StartupScreen(),
